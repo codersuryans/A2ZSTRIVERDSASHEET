1096c1c4bc3fcc009097948f351f61bfa46e471b
codersuryans <mohantysuryans2007@gmail.com>
2025-08-25 19:03:38 +0530

    Add files via upload

---
+++ selectionsort1.java
@@ -0,0 +1,12 @@
+public class selectionsort1{
+    public static void main (Strring args[]){
+        int a[]={7,4,1,5,3};
+        for(int i=0;i<=n-2;i++){
+            
+            for(int j=i;j<=n-1;j++){
+
+            }
+        }
+    
+    }
+}
