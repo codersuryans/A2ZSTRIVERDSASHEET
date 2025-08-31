e3c9ea6ff8b8002d014720c364290075208ddd2d
SURYANS MOHANTY <mohantysuryans2007@gmail.com>
2025-08-31 17:20:39 +0530

    Add files via upload
    MAXIMUM CONSECUTIVE 1'S

---
+++ maxcon.java
@@ -0,0 +1,17 @@
+public class maxcon {
+public static void main(String[] args) {
+    int a[]={1,1,0,1,1,1,0,1,1};
+    int max=0;
+    int count=0;
+    for(int i=0;i<a.length;i++){
+        if(a[i]==1){
+            count++;
+            max=Math.max(max, count);
+        }
+        else{
+            count=0;
+        }
+    }
+    System.out.println(max);
+}
+}
